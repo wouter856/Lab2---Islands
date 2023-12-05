58fba20bff098a250dafe5c2d1b26cae43ecfe5d
wouter856 <wouterwaumans@live.be>
2023-12-05 17:22:40 +0100

    feat: btn binding

--- js/classes/Island.js
+++ js/classes/Island.js
@@ -2,30 +2,31 @@ export default class Island {
     constructor(name) {}
   
     getRandomColor() {
-      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     }
   
     remove() {
-      // JS animations api, fade out
-      // remove the element when the animation ended
+        //JS animations API, fade out and remove from DOM
+        //remove the element when the animation ended
+        
     }
   
     getRandomName() {
       // array with 10 random island names
       const names = [
-        "Palmtree beach",
-        "Sandy beach",
-        "Tropical beach",
-        "Palm beach",
-        "Sunny beach",
-        "Paradise beach",
-        "Sunny island",
-        "Tropical island",
-        "Palm island",
-        "Paradise island"
+            "Daniel",
+            "Bart",
+            "Jeroen",
+            "Luc",
+            "Joris",
+            "Bram",
+            "Jan",
+            "Koen",
+            "Lars",
+            "Jelle"
       ];
   
       // return a random name from the array
+      return names[Math.floor(Math.random() * names.length)];
     }
   }
-  
--- js/classes/World.js
+++ js/classes/World.js
@@ -1,38 +1,62 @@
-class World {
-    constructor() {
-      this.islands = []; // a good place to keep track of your islands
-      this.hookEvents(); // let's kick things of by hooking up events
-    }
-  
-    hookEvents() {
-      // hook events like clicking buttons to a specific function
-    }
-  
-    save() {
-      // save array islands to localstorage as string
-      // loop over all this.islands and save the names
-    }
-  
-    load() {
-      // load islands from localstorage into array
-      // loop over the array and addIslands()
-    }
-  
-    getCoordinates() {
-      // return coordinates within the screen at random, feel free to change it up!
-      let randomSign = Math.random() < 0.5 ? -1 : 1;
-      return {
-        x: ((Math.random() * window.innerWidth) / 2) * randomSign,
-        y: ((Math.random() * window.innerHeight) / 2) * randomSign
-      };
-    }
-  
-    addIsland(island) {
-      // add the islands to the DOM
-    }
-  
-    moveIsland(island) {
-      // this might be a good point to animate the islands with JS Animations API
-    }
+import Island from "./Island.js";
+
+const island = new Island();
+
+export default class World {
+  constructor() {
+    this.islands = []; // a good place to keep track of your islands
+    this.hookEvents(); // let's kick things of by hooking up events
   }
-  
+
+  hookEvents() {
+    // hook events like clicking buttons to a specific function
+    // when clicking on the btn with id #btnAddIsland, call function addIsland()
+    document.querySelector("#btnAddIsland").addEventListener("click", () => {
+        this.addIsland();
+    });
+
+    // when clicking on the btn with id #btnSave, call function save()
+    document.querySelector("#btnSave").addEventListener("click", () => {
+        this.save();
+    });
+
+    // when clicking on the btn with id #btnLoad, call function load()
+    document.querySelector("#btnLoad").addEventListener("click", () => {
+        this.load();
+    });
+  }
+
+  save() {
+    // save array islands to localstorage as string
+    // loop over all this.islands and save the names
+    
+  }
+
+  load() {
+    // load islands from localstorage into array
+    // loop over the array and addIslands()
+    
+  }
+
+  getCoordinates() {
+    // return coordinates within the screen at random, feel free to change it up!
+    let randomSign = Math.random() < 0.5 ? -1 : 1;
+    return {
+      x: ((Math.random() * window.innerWidth) / 2 - 100) * randomSign,
+      y: ((Math.random() * window.innerHeight) / 2 - 100) * randomSign,
+    };
+  }
+
+  addIsland(color, name) {
+    if (!color || !name) {
+      // If color and name are not provided, generate random ones
+      color = island.getRandomColor();
+      name = island.getRandomName();
+    };
+  }
+
+  moveIsland(island) {
+    // this might be a good point to animate the islands with JS Animations API
+    
+  }
+}
